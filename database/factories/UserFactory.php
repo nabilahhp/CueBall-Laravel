d2fb8ac2baebe48044487204c2be7e37a1dc85d3
giayyaaa <agyyaaaaaati@gmail.com>
2024-05-10 21:25:45 +0700

    cobalagi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,4 +1,10 @@
+<?php
 
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
