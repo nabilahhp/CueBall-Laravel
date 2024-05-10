88e4de70e846ef7076facae8e00f512ac404b64d
giayyaaa <agyyaaaaaati@gmail.com>
2024-05-10 21:24:26 +0700

    coba

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,10 +1,4 @@
-<?php
 
-namespace Database\Factories;
-
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
