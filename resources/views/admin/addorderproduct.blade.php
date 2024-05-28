75d4d0052110acd0f6621ba83cc79761251caefb
gitaysn <gitayasinda155@gmail.com>
2024-05-28 12:34:27 +0700

    updatelagi

--- resources/views/admin/addorderproduct.blade.php
+++ resources/views/admin/addorderproduct.blade.php
@@ -105,7 +105,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>&copy; Your Website {{ date('Y') }}</span>
+                        <span>Copyright &copy; Basecamp Billiard {{ date('Y') }}</span>
                     </div>
                 </div>
             </footer>
